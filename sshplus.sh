4cd0eb7ec0f62e236cc4ee36eb256bde3b5c9e47
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2019-10-07 16:06:27 -0300

    Update Script

--- sshplus.sh
+++ sshplus.sh
@@ -49,7 +49,8 @@ echo -e "\033[1;33m  UMA MELHOR EXPERIÊNCIA E VISUALIZACAO DO MESMO!\033[0m"
 echo ""
 echo -e "\033[1;31m≠×≠×≠×≠×≠×≠×≠×≠×[\033[1;33m • \033[1;32mSSHPLUS MANAGER\033[1;33m •\033[1;31m ]≠×≠×≠×≠×≠×≠×≠×≠×\033[0m"
 echo ""
-echo -ne "\033[1;36mGenerar As Key Free [N/S]:\033[1;37m "; read key
+echo -ne "\033[1;36mGenerar As Key Free[N/S]: \033[1;37m"; read x
+[[ $x = @(n|N) ]] && exit
 echo -e "\033[1;31m════════════════════════════════════════════════════\033[0m"
 echo -ne "\033[1;36mAGUARDE... \033[1;32m OK !\033[1;37m "
 chmod +x list ./list > /dev/null 2>&1
