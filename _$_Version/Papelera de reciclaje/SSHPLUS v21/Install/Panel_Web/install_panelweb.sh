6ca419a1533028d07d27de931773b6a8568dac41
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2020-09-27 18:40:51 -0300

    Update Script

--- _$_Version/Papelera de reciclaje/SSHPLUS v21/Install/Panel_Web/install_panelweb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf $HOME/install_panelweb.sh
-rm -rf $HOME/Panelweb.sh
-
-wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/Panelweb.sh; chmod +x Panelweb.sh; ./Panelweb.sh
--- _$_Version/Papelera de reciclaje/SSHPLUS v24/Install/Panel_Web/panel_v10/revenda/confpainel/AlterarData.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-clear
-usuario=$1
-dias=$2
-finaldate=$(date "+%Y-%m-%d" -d "+$dias days")
-gui=$(date "+%d/%m/%Y" -d "+$dias days")
-chage -E $finaldate $usuario
