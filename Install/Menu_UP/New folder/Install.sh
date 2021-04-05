cf3ab7dddd5e6cbee8913ed15bb0b468d9d3673a
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2021-04-05 14:13:56 -0400

    Update Script

--- Install/Menu_UP/New folder/Install.sh
+++ Install/Menu_UP/New folder/Install.sh
@@ -3,12 +3,17 @@
 wget -O /bin/menu https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/menu; chmod +x /bin/menu
 wget -O /bin/usercodes https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/usercodes; chmod +x /bin/usercodes
 wget -O /bin/ferramentascodes https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/ferramentascodes; chmod +x /bin/ferramentascodes
-wget -O /bin/utilitarios https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/utilitarios; chmod +x /bin/utilitarios
-wget -O /bin/criarpay https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/criarpay; chmod +x /bin/criarpay
-wget -O /bin/fai2ban https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/fai2ban; chmod +x /bin/fai2ban
-wget -O /bin/paysnd https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/paysnd; chmod +x /bin/paysnd
-wget -O /bin/squidcache https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/squidcache; chmod +x /bin/squidcache
-wget -O /bin/tcpspeed https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/tcpspeed; chmod +x /bin/tcpspeed
-wget -O /bin/ultrahost https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/ultrahost; chmod +x /bin/ultrahost
-wget -O /bin/vncinstall https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/vncinstall; chmod +x /bin/vncinstall
+
+
+wget -O /bin/menu https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/New%20folder/menu; chmod +x /bin/menu
+wget -O /bin/utilitarios https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/New%20folder/utilitarios; chmod +x /bin/utilitarios
+wget -O /bin/criarpay https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/New%20folder/criarpay; chmod +x /bin/criarpay
+wget -O /bin/fai2ban https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/New%20folder/fai2ban; chmod +x /bin/fai2ban
+wget -O /bin/paysnd https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/New%20folder/paysnd; chmod +x /bin/paysnd
+wget -O /bin/squidcache https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/New%20folder/squidcache; chmod +x /bin/squidcache
+wget -O /bin/tcpspeed https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/New%20folder/tcpspeed; chmod +x /bin/tcpspeed
+wget -O /bin/ultrahost https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/New%20folder/ultrahost; chmod +x /bin/ultrahost
+wget -O /bin/vncinstall https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Menu_UP/New%20folder/vncinstall; chmod +x /bin/vncinstall
 cd && rm -rf Install.sh
+
+# wget -O /bin/menuV2 https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/menuV2; chmod +x /bin/menuV2 > /dev/null 2>&1
