0a99034a82da9cc1f8b3606e477cd26c0835a6c5
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2021-07-28 06:14:59 -0400

    Update Script

--- Install/Multi-Instalador/sshplus.sh
+++ Install/Multi-Instalador/sshplus.sh
@@ -44,7 +44,7 @@ apt-get update -y; apt-get upgrade -y; wget sshplus.xyz/script/Plus; chmod 777 P
 
 #PAINEL WEB SCRIPTS
 panel_v20 () {
-wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v20/install > /dev/null 2>&1; chmod 777 iinstall* && ./install*
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v20/install > /dev/null 2>&1; chmod 777 install* && ./install*
 }
 panel_v23 () {
 wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v23/install > /dev/null 2>&1; chmod +x install; ./install
